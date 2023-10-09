b18dd8f489619f3af0c8e3e00a7810a6db69bd3a
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-09 09:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const modal = document.getElementById("modal");
+
+openModalBtn.addEventListener("click", function() {
+  modal.classList.add("modal-open");
+});
+
+closeModalBtn.addEventListener("click", function() {
+  modal.classList.remove("modal-open");
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target === modal) {
+    modal.classList.remove("modal-open");
+  }
+});
