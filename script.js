7e59a98e60d1970d64b3330cc4f763a13e6f7181
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-09 09:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-document.getElementById('openModalBtn').addEventListener('click', function () {
-    document.getElementById('modal').style.display = 'block';
+var modal = document.getElementById("modal");
+var openModalBtn = document.getElementById("openModal");
+var closeModalBtn = document.querySelector(".close-modal");
+
+openModalBtn.addEventListener("click", function() {
+  modal.style.display = "block";
 });
 
-document.getElementById('closeModalBtn').addEventListener('click', function () {
-    document.getElementById('modal').style.display = 'none';
+closeModalBtn.addEventListener("click", function() {
+  modal.style.display = "none";
 });
 
-window.addEventListener('click', function (event) {
-    if (event.target === document.getElementById('modal')) {
-        document.getElementById('modal').style.display = 'none';
-    }
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 });
